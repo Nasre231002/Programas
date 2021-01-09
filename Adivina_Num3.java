cd71dd6448834a1e07a46841a7d1b3ef9071997a
Nasre231002 <77195893+Nasre231002@users.noreply.github.com>
2021-01-09 13:20:34 +0100

    Add files via upload

---
+++ Adivina_Num3.java
@@ -0,0 +1,43 @@
+package Bucles;
+import java.util.Scanner;
+
+/**
+ * Write a description of class Adivina_Num3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Adivina_Num3 {
+    public static void main (String[] args){
+    Scanner entrada = new Scanner(System.in);
+    int numero = (int)(Math.random() * 10 ); 
+    System.out.println("El numero es " + numero );
+    int i =0 ;
+    for (i = 0 ;i<10;i--){
+    System.out.println("¿Es correcto el numero?");
+    String respuesta = entrada.nextLine();
+    if (respuesta.equalsIgnoreCase("Si")){
+    
+    System.out.println ("Correcto");
+    break;
+    
+    }else if (respuesta.equalsIgnoreCase("No,es mas alto")){
+    
+    numero++ ;
+    System.out.println("Es el "  + numero + "?");
+    
+    }else if (respuesta.equalsIgnoreCase("No,es mas bajo")){
+    numero--;
+    System.out.println("Es el " + numero + "?");
+    
+    }
+     
+       
+        
+        
+     }
+  }
+     
+    
+}
+
